eb41e783f0df5c917e7c15f6d6919b1761532749
Shah <muhsinshah21@gmail.com>
2021-02-08 00:21:23 +0530

    added  login

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.use((req,res,next)=>{
 
 app.use('/auth',authRouter);
 
+app.use((error,req,res,next)=>{
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({message:message,data:data});
+}); 
 
 db.connectDb(()=>{
     console.log('Databse connection successfull...');
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,44 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require('bcryptjs');
 const User = require("../models/user");
+const jwt = require('jsonwebtoken');
 
 
-exports.postLogin = (req,res,next)=>{
+exports.postLogin = async (req,res,next)=>{
     const email = req.body.email;
     const password = req.body.password;
+
+    try{
+        const user = await User.findByEmail(email);
+        if (!user) {
+          const error = new Error("Incorrect Email");
+          error.statusCode = 422;
+          error.data = errors;
+          throw error;
+        }
+
+        const isPasswordsEqual = await bcrypt.compare(password,user.password);
+        if(!isPasswordsEqual){
+            const error = new Error('Incorrect Password');
+            error.statusCode = 422;
+            error.data = errors;
+            throw error;    
+        }
+
+        const token = jwt.sign({
+            email:user.email,
+            userId:user._id,
+        },'youwillneverwalkalone',{expiresIn:'1hr'});
+
+        res.status(201).json({messge:'login successfull.',token:token,userId:user._id});
+    }catch(err){
+        console.log(err);
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+
 }
 
 exports.postSignup = async (req,res,next)=>{
@@ -15,13 +48,15 @@ exports.postSignup = async (req,res,next)=>{
     const password = req.body.password;
     const errors = validationResult(req).array();
 
-    if(errors.length > 0){
-        const error = new Error('Invalid data');
-        error.statusCode = 422;
-        throw error;    
-    }
-
     try{
+
+        if(errors.length > 0){
+            const error = new Error('Invalid data');
+            error.statusCode = 422;
+            error.data = errors;
+            throw error;    
+        }
+
         const hashedPass = await bcrypt.hash(password,12);
         const newUser = new User(email,hashedPass,Date.now());
         const result = await newUser.save();
@@ -30,14 +65,8 @@ exports.postSignup = async (req,res,next)=>{
         if(!err.statusCode){
             err.statusCode = 500;
         }
-        throw err;
+        next(err);
     }
-    
-
-
-
-    
-
 
 }
 
