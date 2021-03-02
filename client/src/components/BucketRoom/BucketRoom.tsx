e84eb39395fcdb7dd301991997753cf628fe89f0
Shah <muhsinshah21@gmail.com>
2021-03-02 21:54:07 +0530

    added bucket room

--- client/src/components/BucketRoom/BucketRoom.tsx
+++ client/src/components/BucketRoom/BucketRoom.tsx
@@ -1,5 +1,7 @@
 import React,{useMemo, useState} from 'react'
-import { TextField } from "@material-ui/core";
+import { Button,TextField } from "@material-ui/core";
+import Send from "@material-ui/icons/Send";
+import {makeStyles,createMuiTheme,MuiThemeProvider } from "@material-ui/core/styles"
 
 import './BucketRoom.css'
 import axios from "../../axios/config";
@@ -34,9 +36,33 @@ const getBucket = async (id:string,userToken:any) =>{
 
 }
 
+
+const useStyle = makeStyles({   
+    cssLabel: {
+        color: 'rgb(61, 158, 116) !important'
+    },
+    notchedOutline: {
+        borderWidth: '1px',
+        borderColor: 'rgb(61, 158, 116) !important',
+        color: 'rgb(61, 158, 116)',
+    },
+    // sendButton:{
+    //     color:'#fff',
+    // },
+    button:{
+        width:'100px',
+        height:'40px',
+        background:'#32be8f',
+    }
+});
+
 function BucketRoom(props:propTypes) {
 
     const [bucket,setBucket] = useState<Bucket>(null!);
+
+   
+
+    const classes = useStyle();
 
     useMemo(()=>{
         async function promiseList(){
@@ -53,15 +79,38 @@ function BucketRoom(props:propTypes) {
     return (
         <div className="bucket">
             {bucket &&(
-                <div className="room">
-                    <div className="headerSection">
-                        <h4>{bucket.name}</h4>
+                <div>
+                    <div className="room">
+                        <div className="headerSection">
+                            <h4>{bucket.name}</h4>
+                        </div>
+                        <div className="contents"></div>
                     </div>
-                    <div className="contents"></div>
                     <div className="inputSection">
-                        
-                        <textarea/>
 
+                        <TextField margin="dense" label="Type here" 
+                        InputLabelProps={{
+                            classes: {
+                              root: classes.cssLabel,
+                              focused: classes.cssLabel,
+                            },
+                        }}
+                        
+                        InputProps={{
+                            classes: {
+                              root: classes.notchedOutline,
+                              focused: classes.notchedOutline,
+                              notchedOutline: classes.notchedOutline,
+                            },
+                            
+                         }}
+
+                        variant="outlined" className="input" />
+                        
+                        <Button className={classes.button}>
+                            <Send/>
+                        </Button>
+                        
                     </div>
                 </div>
             )}
